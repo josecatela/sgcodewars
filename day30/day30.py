5b52c5bb31b587b0313c7907d490fdf28d66aae0
Oleksandra Chmel <35497059+alexachmel@users.noreply.github.com>
2020-02-26 06:05:18 +0200

    Return 1, 2, 3 randomly

--- day30/day30.py
+++ day30/day30.py
@@ -1,6 +1,6 @@
 import timeit
 import random
-from collections import Counter
+
 
 def one_two():
     return random.randrange(1,3)
@@ -35,6 +35,21 @@ TEST_CODE_ccquiel = '''
 result = ccquiel_day30()
 '''
 
+charlie_ang_setup = '''
+from __main__ import charlie_ang_day30
+'''
+
+def charlie_ang_day30():
+    while True:
+        if one_two() == 1:
+            return one_two()
+        elif one_two() == 1:
+            return 3
+
+TEST_CODE_charlie_ang = '''
+result = charlie_ang_day30()
+'''
+
 diana_henninger_setup = '''
 from __main__ import diana_henninger_day30
 '''
@@ -49,6 +64,22 @@ def diana_henninger_day30():
 
 TEST_CODE_diana_henninger = '''
 result = diana_henninger_day30()
+'''
+
+Jens_setup = '''
+from __main__ import Jens_day30
+'''
+
+def Jens_day30():
+    while True:
+        r_list = [i for i in range(1, 4) if one_two() == 1]
+        if len(r_list) == 1:
+            return r_list[0]
+        elif len(r_list) == 2:
+            return r_list[0] if one_two() == 1 else r_list[1]
+
+TEST_CODE_Jens = '''
+result = Jens_day30()
 '''
 
 Kurt_Hinderer_setup = '''
@@ -71,39 +102,6 @@ TEST_CODE_Kurt_Hinderer = '''
 result = Kurt_Hinderer_day30()
 '''
 
-Jens_setup = '''
-from __main__ import Jens_day30
-'''
-
-def Jens_day30():
-    while True:
-        r_list = [i for i in range(1, 4) if one_two() == 1]
-        if len(r_list) == 1:
-            return r_list[0]
-        elif len(r_list) == 2:
-            return r_list[0] if one_two() == 1 else r_list[1]
-
-TEST_CODE_Jens = '''
-result = Jens_day30()
-'''
-
-Yang_setup = '''
-from __main__ import Yang_day30
-'''
-
-def Yang_day30(): 
-    a,b = one_two(),one_two()+1
-    if a*b==4: 
-        return 1 
-    elif a*b<4: 
-        return a*b
-    else: 
-        return Yang_day30() 
-
-TEST_CODE_Yang = '''
-result = Yang_day30()
-'''
-
 Oleksandra_Chmel_setup = '''
 from __main__ import Oleksandra_Chmel_day30
 '''
@@ -123,21 +121,6 @@ TEST_CODE_Oleksandra_Chmel = '''
 result = Oleksandra_Chmel_day30()
 '''
 
-charlie_ang_setup = '''
-from __main__ import charlie_ang_day30
-'''
-
-def charlie_ang_day30():
-    while True:
-        if one_two() == 1:
-            return one_two()
-        elif one_two() == 1:
-            return 3
-
-TEST_CODE_charlie_ang = '''
-result = charlie_ang_day30()
-'''
-
 Tushar_Jain_setup = '''
 from __main__ import Tushar_Jain_day30
 '''
@@ -153,24 +136,28 @@ TEST_CODE_Tushar_Jain = '''
 result = Tushar_Jain_day30()
 '''
 
-#print("Time for akash_karan test code: " + str(timeit.timeit(stmt=TEST_CODE_akash_karan, setup=akash_karan_setup, number=10000)) + " seconds")
-#print("Time for akash_agarwal test code: " + str(timeit.timeit(stmt=TEST_CODE_akash_agarwal, setup=akash_agarwal_setup, number=10000)) + " seconds")
-#print("Time for Samrat_Mukherjee test code: " + str(timeit.timeit(stmt=TEST_CODE_Samrat_Mukherjee, setup=Samrat_Mukherjee_setup, number=10000)) + " seconds")
-#print("Time for kilian test code: " + str(timeit.timeit(stmt=TEST_CODE_kilian, setup=kilian_setup, number=10000)) + " seconds")
+Yang_setup = '''
+from __main__ import Yang_day30
+'''
+
+def Yang_day30(): 
+    a,b = one_two(),one_two()+1
+    if a*b==4: 
+        return 1 
+    elif a*b<4: 
+        return a*b
+    else: 
+        return Yang_day30() 
+
+TEST_CODE_Yang = '''
+result = Yang_day30()
+'''
+
 print("Time for ccquiel test code: " + str(timeit.timeit(stmt=TEST_CODE_ccquiel, setup=ccquiel_setup, number=100000)) + " seconds")
-#print("Time for vijaya_lakshmi test code: " + str(timeit.timeit(stmt=TEST_CODE_vijaya_lakshmi, setup=vijaya_lakshmi_setup, number=10000)) + " seconds")
-print("Time for diana_henninger test code: " + str(timeit.timeit(stmt=TEST_CODE_diana_henninger, setup=diana_henninger_setup, number=100000)) + " seconds")
-#print("Time for ggebre test code: " + str(timeit.timeit(stmt=TEST_CODE_ggebre, setup=ggebre_setup, number=10000)) + " seconds")
-#print("Time for Krzysztof_Blach test code: " + str(timeit.timeit(stmt=TEST_CODE_Krzysztof_Blach, setup=Krzysztof_Blach_setup, number=10000)) + " seconds")
-#print("Time for Prashanth_Kadimisetty test code: " + str(timeit.timeit(stmt=TEST_CODE_Prashanth_Kadimisetty, setup=Prashanth_Kadimisetty_setup, number=10000)) + " seconds")
-print("Time for Kurt_Hinderer test code: " + str(timeit.timeit(stmt=TEST_CODE_Kurt_Hinderer, setup=Kurt_Hinderer_setup, number=100000)) + " seconds")
-#print("Time for Jose_Catela test code: " + str(timeit.timeit(stmt=TEST_CODE_Jose_Catela, setup=Jose_Catela_setup, number=10000)) + " seconds")
-print("Time for Jens test code: " + str(timeit.timeit(stmt=TEST_CODE_Jens, setup=Jens_setup, number=100000)) + " seconds")
-print("Time for Yang test code: " + str(timeit.timeit(stmt=TEST_CODE_Yang, setup=Yang_setup, number=100000)) + " seconds")
-#print("Time for Vanessa_G test code: " + str(timeit.timeit(stmt=TEST_CODE_Vanessa_G, setup=Vanessa_G_setup, number=10000)) + " seconds")
-#print("Time for Daniel_Ruiz test code: " + str(timeit.timeit(stmt=TEST_CODE_Daniel_Ruiz, setup=Daniel_Ruiz_setup, number=10000)) + " seconds")
-#print("Time for Memo_Hurtado test code: " + str(timeit.timeit(stmt=TEST_CODE_Memo_Hurtado, setup=Memo_Hurtado_setup, number=10000)) + " seconds")
-print("Time for Oleksandra_Chmel test code: " + str(timeit.timeit(stmt=TEST_CODE_Oleksandra_Chmel, setup=Oleksandra_Chmel_setup, number=100000)) + " seconds")
-#print("Time for sjay test code: " + str(timeit.timeit(stmt=TEST_CODE_sjay, setup=sjay_setup, number=10000)) + " seconds")
 print("Time for charlie_ang test code: " + str(timeit.timeit(stmt=TEST_CODE_charlie_ang, setup=charlie_ang_setup, number=100000)) + " seconds")
+print("Time for diana_henninger test code: " + str(timeit.timeit(stmt=TEST_CODE_diana_henninger, setup=diana_henninger_setup, number=100000)) + " seconds")
+print("Time for Jens test code: " + str(timeit.timeit(stmt=TEST_CODE_Jens, setup=Jens_setup, number=100000)) + " seconds")
+print("Time for Kurt_Hinderer test code: " + str(timeit.timeit(stmt=TEST_CODE_Kurt_Hinderer, setup=Kurt_Hinderer_setup, number=100000)) + " seconds")
+print("Time for Oleksandra_Chmel test code: " + str(timeit.timeit(stmt=TEST_CODE_Oleksandra_Chmel, setup=Oleksandra_Chmel_setup, number=100000)) + " seconds")
 print("Time for Tushar_Jain test code: " + str(timeit.timeit(stmt=TEST_CODE_Tushar_Jain, setup=Tushar_Jain_setup, number=100000)) + " seconds")
+print("Time for Yang test code: " + str(timeit.timeit(stmt=TEST_CODE_Yang, setup=Yang_setup, number=100000)) + " seconds")
